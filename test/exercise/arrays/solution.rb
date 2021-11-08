ac35c5866f60260e93d1645d9c15aa48f86b7433
Anton <web-gambit@mail.ru>
2021-11-08 17:38:40 +0500

    Refactor binary search

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -14,31 +14,21 @@ module Exercise
       end
 
       def search(array, item)
-        bs_iter = lambda do |array_slice, indexes|
-          return -1 if array_slice.empty?
+        bs_iter = lambda do |left_bound_index, right_bound_index|
+          return -1 if array.empty? || left_bound_index > right_bound_index
 
-          middle_item_index = indexes.size / 2
-          middle_item = array_slice[middle_item_index]
+          middle_item_index = (left_bound_index + right_bound_index) / 2
+          middle_item = array[middle_item_index]
 
-          return indexes[middle_item_index] if item == middle_item
+          return middle_item_index if item == middle_item
 
-          left_bound_index = 0
-          right_bound_index = indexes[-1]
-          return -1 if left_bound_index == right_bound_index
+          left_bound_index = middle_item_index + 1 if item > middle_item
+          right_bound_index = middle_item_index - 1 if item < middle_item
 
-          if item > middle_item
-            left_bound_index = middle_item_index + 1
-          elsif item < middle_item
-            right_bound_index = middle_item_index - 1
-          end
-          return bs_iter.call(
-            array_slice[left_bound_index..right_bound_index],
-            indexes[left_bound_index..right_bound_index]
-          )
+          return bs_iter.call(left_bound_index, right_bound_index)
         end
 
-        indexes = (0..array.size - 1).to_a
-        bs_iter.call(array, indexes)
+        bs_iter.call(0, array.size - 1)
       end
     end
   end
