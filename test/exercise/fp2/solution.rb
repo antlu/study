bdb8f6591ef77a49043110464b0765456350a5a1
Anton <web-gambit@mail.ru>
2021-11-10 02:53:39 +0500

    Do exercise with fp2

--- test/exercise/fp2/solution.rb
+++ test/exercise/fp2/solution.rb
@@ -5,16 +5,35 @@ module Exercise
       # Использовать свои написанные функции для реализации следующих - можно.
 
       # Написать свою функцию my_each
-      def my_each; end
+      def my_each
+        iter = lambda do |(first, *rest)|
+          yield first
+
+          return if rest.empty?
+
+          return iter.call(rest)
+        end
+        iter.call(self)
+        self
+      end
 
       # Написать свою функцию my_map
-      def my_map; end
+      def my_map(&block)
+        my_reduce(self.class.new) { |result, e| result << block.call(e) }
+      end
 
       # Написать свою функцию my_compact
-      def my_compact; end
+      def my_compact
+        my_reduce(self.class.new) { |result, e| e.nil? ? result : result << e }
+      end
 
       # Написать свою функцию my_reduce
-      def my_reduce; end
+      def my_reduce(start_value = nil, &block)
+        result = start_value || first
+        array = start_value.nil? ? self[1..] : self
+        array.my_each { |e| result = block.call(result, e) }
+        result
+      end
     end
   end
 end
